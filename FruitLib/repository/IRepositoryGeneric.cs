2c66f45536e6331f579b4d8bdde357987d68acef
Peter Levinsky <pele@zealand.dk>
2024-02-18 18:19:56 +0100

    Generic-trin-1

---
+++ FruitLib/repository/IRepositoryGeneric.cs
@@ -0,0 +1,18 @@
+﻿using FruitLib.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitLib.repository
+{
+    public interface IRepositoryGeneric<T>
+    {
+        List<T> ReadAll();      // eller GetAll
+        T ReadById(int id);     // eller GetById
+        T Create(T item);  // eller Add
+        T Delete(int id);       // eller Remove
+        T Update(int id, T item); // eller Modify
+    }
+}
