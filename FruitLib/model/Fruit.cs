9400d7289e34f21ecc56f152a524d38bf907cb50
Peter Levinsky <pele@zealand.dk>
2024-02-18 18:22:28 +0100

    Generic-trin-2

--- FruitLib/model/Fruit.cs
+++ FruitLib/model/Fruit.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FruitLib.repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace FruitLib.model
 {
-    public class Fruit
+    public class Fruit:IModel
     {
         public int Id { get; set; }
         public string Name { get; set; }
